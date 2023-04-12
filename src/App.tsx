f26780b32bed395d92063586a79d5c1e557dff58
Josh <joshuatylermartinez@gmail.com>
2023-04-12 18:28:24 -0400

    Split up files so that state is shared between them. Updated UserSelect name to be UserDropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { UserSelect } from "./Components/UserSelect";
+import { UserDropDown } from "./Components/UserDropdown";
+//import { User } from "./Components/UserSelect";
+//import { userType, setUserType } from "./Components/UserSelect";
 
 function App(): JSX.Element {
+    const [userType, setUserType] = useState<string>("superUser");
     return (
         <div className="App">
             <header className="App-header">
@@ -12,7 +15,21 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <UserSelect></UserSelect>
+            <UserDropDown
+                userType={userType}
+                setUserType={setUserType}
+            ></UserDropDown>
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: "red",
+                    display: userType === "Admin" ? "inline-block" : "none",
+                    verticalAlign: "bottom",
+                    marginLeft: "5px"
+                }}
+            ></div>
         </div>
     );
 }
---
+++ src/Components/UserDropdown.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+export interface userTypeProps {
+    userType: string;
+    setUserType: (newUserType: string) => void;
+}
+
+export function UserDropDown({
+    userType,
+    setUserType
+}: userTypeProps): JSX.Element {
+    function updateUserType(event: React.ChangeEvent<HTMLSelectElement>) {
+        setUserType(event.target.value);
+    }
+
+    return (
+        <div>
+            <Form.Group controlId="userEmotions">
+                <Form.Label>How do you feel?</Form.Label>
+                <Form.Select value={userType} onChange={updateUserType}>
+                    <option value="superUser">superUser</option>
+                    <option value="Admin">Admin</option>
+                    <option value="User">User</option>
+                </Form.Select>
+            </Form.Group>
+            The user is {userType}.
+        </div>
+    );
+}
