404a57ca9259c44f38e0dd60852664d7f448f365
Josh <joshuatylermartinez@gmail.com>
2023-05-06 18:53:42 -0400

    removed unused file and element call

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import {
     AdminSelectButton
 } from "./Components/UserTypeSelectButtons";
 import { User } from "./Interfaces/UserObject";
-import { DropBox } from "./Components/Draggable";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { CenterList } from "./Components/Centerlist";
@@ -102,7 +101,6 @@ function App(): JSX.Element {
                     mealList={mealList}
                     setMealList={setMealList}
                 ></CenterList>
-                <DropBox></DropBox>
                 <UserList
                     currentUser={currentUser}
                     setCurrentUser={setCurrentUser}
