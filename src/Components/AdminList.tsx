cfc2f8843533547898b3b58b60718bff90cc6968
Sreya <sreya@udel.edu>
2023-05-08 15:23:02 -0400

    flip visibility for admin list

---
+++ src/Components/AdminList.tsx
@@ -0,0 +1,35 @@
+// import React, { useState } from "react";
+// import { Form } from "react-bootstrap";
+// import { userTypeProps } from "./UserDropdown";
+
+// const brands=[[10,90,87],[11,20,91],[8,40,5]]
+// const gitem={
+//     name: string;
+//     nutrition: int;
+// }
+// export function adminList({
+//     name,
+//     nutrition,
+// }: {
+//     name: string[];
+//     nutrition: int;
+// }): JSX.Element {
+//     const [nutList, setnutList] = useState<Array>(brands[0]);
+// }
+
+//     const adminList = centerList.map((nut: number): number => price*2);
+// console.log(doubled);
+
+// function adminView(): JSX.Element {
+//     const [visible, setVisible] = useState<userTypeProps>("Admin");
+
+//     function flipVisibility(): void {
+//         setVisible(!visible);
+//     }
+//     return (
+//         <div>
+//             <Button onClick={flipVisibility}>Show/Hide</Button>
+//             {visible && <div>adminList</div>}
+//         </div>
+//     );
+// }
