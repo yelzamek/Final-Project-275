7dc3272e3caa3ca0ac18384e071d41bb954c409f
Sreya <sreya@udel.edu>
2023-05-11 16:15:03 -0400

    working on meal error messages

--- src/Components/adminList.tsx
+++ src/Components/adminList.tsx
@@ -1,100 +1,35 @@
-/* eslint-disable no-extra-parens */
-import { UserTypeProps } from "../Interfaces/UserTypeProps";
-import { useDrop } from "react-dnd";
-import { Button } from "react-bootstrap";
-import { AdminListProps } from "../Interfaces/AdminListProps";
-import { Meal, MealListProps, nameProps } from "../Interfaces/MealObject";
-import { EditMeal } from "./EditMeal";
-import React, { useState } from "react";
-//import { AdminEditHiddenProps } from "../Interfaces/AdminEditHiddenProps";
-import { UserListProps } from "../Interfaces/UserListProps";
+// import React, { useState } from "react";
+// import { Form } from "react-bootstrap";
+// import { userTypeProps } from "./UserDropdown";
 
-export function AdminList({
-    mealList,
-    setMealList,
-    userType,
-    adminList,
-    setAdminList,
-    userList,
-    setUserList
-}: MealListProps &
-    UserTypeProps &
-    AdminListProps &
-    UserListProps): JSX.Element {
-    const [editHidden, setEditHidden] = useState<boolean>(true);
+// const brands=[[10,90,87],[11,20,91],[8,40,5]]
+// const gitem={
+//     name: string;
+//     nutrition: int;
+// }
+// export function adminList({
+//     name,
+//     nutrition,
+// }: {
+//     name: string[];
+//     nutrition: int;
+// }): JSX.Element {
+//     const [nutList, setnutList] = useState<Array>(brands[0]);
+// }
 
-    function addToAdminList(name: nameProps) {
-        const mealIndex = mealList.findIndex(
-            (meal: Meal): boolean => meal.name === name.name
-        );
-        if (!adminList.includes(mealList[mealIndex])) {
-            setAdminList([...adminList, mealList[mealIndex]]);
-        }
-    }
-    const [{ isOver }, drop] = useDrop({
-        accept: "Meal",
-        drop: (name: nameProps) => addToAdminList(name),
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver()
-        })
-    });
-    function RemoveItem(item: Meal, index: number) {
-        const updatedList = [...adminList];
-        updatedList.splice(index, 1);
-        setAdminList(updatedList);
-    }
-    return (
-        <div
-            ref={drop}
-            style={{
-                display:
-                    userType === "Admin" || userType === "superUser"
-                        ? "inline-block"
-                        : "none",
-                backgroundColor: isOver ? "lightgray" : "white",
-                width: "300px",
-                height: "auto",
-                border: "2px solid black"
-            }}
-        >
-            <div>Admin List:</div>
-            {adminList.map((item: Meal, index: number) => (
-                <div key={index}>
-                    {item.name}{" "}
-                    <div>
-                        <Button onClick={() => RemoveItem(item, index)}>
-                            Remove
-                        </Button>
-                        <Button onClick={() => setEditHidden(!editHidden)}>
-                            Edit Item
-                        </Button>
-                        <div hidden={editHidden}>
-                            <EditMeal
-                                mealList={mealList}
-                                setMealList={setMealList}
-                                adminList={adminList}
-                                setAdminList={setAdminList}
-                                hidden={editHidden}
-                                setHidden={setEditHidden}
-                                userList={userList}
-                                setUserList={setUserList}
-                                name={item.name}
-                                image={item.image}
-                                serving_size={item.serving_size}
-                                calories={item.calories}
-                                total_fat={item.total_fat}
-                                cholesterol={item.cholesterol}
-                                sodium={item.sodium}
-                                total_carbs={item.total_carbs}
-                                total_sugars={item.total_sugars}
-                                protein={item.protein}
-                                tags={[...item.tags]}
-                                ingredients={[...item.ingredients]}
-                            ></EditMeal>
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-}
+//     const adminList = centerList.map((nut: number): number => price*2);
+// console.log(doubled);
+
+// function adminView(): JSX.Element {
+//     const [visible, setVisible] = useState<userTypeProps>("Admin");
+
+//     function flipVisibility(): void {
+//         setVisible(!visible);
+//     }
+//     return (
+//         <div>
+//             <Button onClick={flipVisibility}>Show/Hide</Button>
+//             {visible && <div>adminList</div>}
+//         </div>
+//     );
+// }
