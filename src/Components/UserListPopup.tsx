7c2efc9e47a159caf2548a18f00d23980d9d1f17
Devin <devinjc@udel.edu>
2023-05-17 15:22:05 -0400

    Created pop up buttons for items in user lists

---
+++ src/Components/UserListPopup.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-extra-parens */
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import "./UsersWithItemPopup.css";
+import { Meal, nameProps } from "../Interfaces/MealObject";
+import { MealListProps } from "../Interfaces/MealObject";
+
+export function ItemPopUp({
+    name,
+    mealList
+}: nameProps & MealListProps): JSX.Element {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleOpenPopup = () => {
+        setShowPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+
+    return (
+        <div>
+            <Button id="itemOpenPopup" onClick={handleOpenPopup}>
+                Open Popup
+            </Button>
+
+            {showPopup && (
+                <div id="itemPopupOverlay" className="show">
+                    <div id="itemPopupContent">
+                        <h2>{name}</h2>
+                        <Button id="itemClosePopup" onClick={handleClosePopup}>
+                            Close
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
